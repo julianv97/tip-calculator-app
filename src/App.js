3d638bf6e899b4da79a609e44cf135ca749024e7
Julian Vicente <julian.vicente97s@gmail.com>
2021-08-16 14:32:59 -0300

    header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 //Components
 import Input from "./components/Input";
 import Output from "./components/Output";
+import Header from "./components/Header";
 
 //State-Context
 import BillState from "./context/BillState";
@@ -11,9 +12,7 @@ function App() {
   return (
     <BillState>
       <div className=" bg-Light-grayish-cyan  w-full h-full min-h-screen flex flex-col items-center font-spacemono pb-10 ">
-        <header className=" flex items-center justify-center h-20 w-full">
-          <img src="images/logo.svg" alt="" />
-        </header>
+        <Header />
         <main className="bg-white border rounded-xl h-full  w-full md:w-8/12  md:h-auto px-6">
           <Input />
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <header className=" flex items-center justify-center h-20 w-full">
+        <img src="images/logo.svg" alt="" />
+      </header>
+    </div>
+  );
+};
+
+export default Header;
